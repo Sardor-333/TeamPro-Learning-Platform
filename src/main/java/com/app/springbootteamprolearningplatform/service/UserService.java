a4575c9a7056be898f8c7e89cb3ec0b93614dd45
m1lymoe17 <muydinovumid2005@gmail.com>
2022-03-04 04:15:36 +0500

    course buy added

--- src/main/java/com/app/springbootteamprolearningplatform/service/UserService.java
+++ src/main/java/com/app/springbootteamprolearningplatform/service/UserService.java
@@ -2,8 +2,10 @@ package com.app.springbootteamprolearningplatform.service;
 
 import com.app.springbootteamprolearningplatform.dto.UserDto;
 import com.app.springbootteamprolearningplatform.model.Attachment;
+import com.app.springbootteamprolearningplatform.model.Course;
 import com.app.springbootteamprolearningplatform.model.Role;
 import com.app.springbootteamprolearningplatform.model.User;
+import com.app.springbootteamprolearningplatform.repository.CourseRepository;
 import com.app.springbootteamprolearningplatform.repository.RoleRepository;
 import com.app.springbootteamprolearningplatform.repository.UserRepository;
 import com.app.springbootteamprolearningplatform.utils.Util;
@@ -18,25 +20,30 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Transactional
 public class UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private CourseRepository courseRepository;
 
     @Autowired
     public UserService(UserRepository userRepository,
-                       RoleRepository roleRepository) {
+                       RoleRepository roleRepository,
+                       CourseRepository courseRepository
+    ) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.courseRepository = courseRepository;
     }
 
     public boolean registerUser(UserDto userDto) {
         if (
                 !userRepository.existsByEmail(userDto.getEmail())
-                        && Util.isValidEmail(userDto.getEmail())
-                        && userDto.getPassword().equals(userDto.getConfirmPassword())) {
+                && Util.isValidEmail(userDto.getEmail())
+                && userDto.getPassword().equals(userDto.getConfirmPassword())) {
 
             User user = new User(
                     userDto.getFirstName(),
@@ -105,6 +112,10 @@ public class UserService {
         return userRepository.findAllByRoles(roleRepository.findByName("MENTOR").orElse(null));
     }
 
+    public User getUserById(UUID id) {
+        return userRepository.getById(id);
+    }
+
     private Attachment getAttachment(MultipartFile multipartFile) {
         try {
             if (multipartFile != null && multipartFile.getSize() != 0) {
@@ -116,5 +127,28 @@ public class UserService {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String buyCourse(UUID userId, UUID courseId) {
+        User user = userRepository.getById(userId);
+        Course course = courseRepository.getById(courseId);
+        if (course.getPrice() < user.getBalance()) {
+            user.setBalance(user.getBalance() - course.getPrice());
+
+            List<User> authors = course.getAuthors();
+            double v = course.getPrice() / authors.size();
+            for (User author : authors) {
+                author.setBalance(author.getBalance() + v);
+                userRepository.save(author);
+            }
+
+            List<Course> userCourses = user.getUserCourses();
+            userCourses.add(course);
+            user.setUserCourses(userCourses);
+            userRepository.save(user);
+            return "You successfully bought this course!";
+        } else {
+            return "You don`t have much money, Fill your balance!";
+        }
     }
 }
